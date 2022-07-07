b0ae6cf16b3a8fb4a511f4e1681f07316b327011
François Sarradin <fsarradin@gmail.com>
2022-07-07 17:14:50 +0200

    add enum content

--- src/main/scala/io/univalence/ws_scala3/20-enum.scala
+++ src/main/scala/io/univalence/ws_scala3/20-enum.scala
@@ -25,13 +25,35 @@ object _01_simple_enum {
    *
    * The enum below just represents some colors.
    */
-  enum Color {
-    case RED, GREEN, BLUE
+  /**
+   * In Scala 2 generally, we tend to write an enumeration as follows.
+   */
+  sealed trait OldColor
+  object OldColor {
+    case object RED   extends OldColor
+    case object GREEN extends OldColor
+    case object BLUE  extends OldColor
   }
+
+  /** In Scala 3, we have Scala enums to replace the syntax. */
+  enum NewColor:
+    case RED extends NewColor
+    case GREEN extends NewColor
+    case BLUE extends NewColor
+
+  /** The notation can be shortened. */
+  enum Color:
+    case RED, GREEN, BLUE
+
+  /**
+   * Info: in reality, Scala enums is a syntaxic sugar that generate
+   * under the hood for our old Scala 2 way of describing enumeration
+   * with several helper functions.
+   */
 
   @main
   def _01_Colors(): Unit =
-    section("PART 1 - Color enumeration") {
+    section("PART 1 - color enumeration") {
       println(s"Red: ${Color.RED}")
 
       /**
@@ -63,11 +85,45 @@ object _01_simple_enum {
   enum JavaColor extends java.lang.Enum[JavaColor] {
     case RED, GREEN, BLUE
   }
-
 }
 
-object _02_enum_with_constructor {
+object _02_arithmetic_expression {
 
+  /**
+   * Scala enums such as Colors is composed by singleton cases.
+   *
+   * But we can also have parametrize cases. It allows us to create
+   * ADTs.
+   *
+   * For the example we will create an ADT that describe a function such
+   * as: f(x) = x + x + become Add(Add(Var,Var),Const(10))
+   */
+  enum Arithmetic:
+    case Var
+    case Const(a: Int)
+    case Add(left: Arithmetic, right: Arithmetic)
+
+    def +(other: Arithmetic): Add = Add(this, other)
+
+    def apply(x: Int): Int =
+      this match
+        case Var       => x
+        case Const(v)  => v
+        case Add(l, r) => l(x) + r(x)
+
+  @main
+  def _02_Arithmetic(): Unit =
+    section("PART 2 - arithmetic expression") {
+      import Arithmetic.*
+
+      val f = Var + Var + Const(10)
+      check(f(20) == ??)
+    }
+}
+
+object _03_enum_with_constructor {
+
+  /** We can parametrize enumeration. */
   enum Note(val frequency: Double) {
     case C4 extends Note(261.63)
     case D4 extends Note(293.66)
@@ -76,18 +132,30 @@ object _02_enum_with_constructor {
     case G4 extends Note(392.0)
     case A4 extends Note(440.0)
     case B4 extends Note(493.88)
+
+    /**
+     * The Note parameters allows us to create generic functions that
+     * works for any Notes.
+     */
+    def time: Double = 1.0 / frequency.toFloat
   }
 
   @main
-  def _02_Notes(): Unit =
-    section("PART 2 - enum with constructor") {
+  def _03_Notes(): Unit =
+    section("PART 3 - enum with constructor") {
       check(Note.A4.frequency == ??)
     }
-
 }
 
-object _03_adt {
+object _04_adt {
 
+  /**
+   * Enumeration in Scala also support type parameters implying the
+   * creation of GADTs.
+   *
+   * As an example, we can reproduce the Option ADT using Scala 3
+   * enumerations.
+   */
   enum PeutEtre[+A] {
     case Present(a: A)
     case Rien
@@ -96,13 +164,46 @@ object _03_adt {
       this match
         case Rien => false
         case _    => true
+
+    def orElse[B >: A](b: B): B =
+      this match
+        case Rien       => b
+        case Present(v) => v
   }
 
   @main
-  def _03_ADT_with_enum(): Unit =
-    section("PART 3 - ADT") {
-      check(PeutEtre.Present("123").isDefined == ??)
-      check(PeutEtre.Rien.isDefined == ??)
+  def _04_ADT_with_enum(): Unit =
+    section("PART 4 - GADT") {
+      import PeutEtre.*
+
+      check(Present("123").isDefined == ??)
+      check(Rien.isDefined == ??)
+      check(Rien.orElse(10) == ??)
     }
+
+  /**
+   * However enumerations don't replace entirely the ADTs created using
+   * sealed traits. Indeed, for example, you can't specify custom
+   * functions for each enumeration cases.
+   */
+
+  // This code doesn't not compile.
+  //enum Cases:
+  //  case FIRST {
+  //    def doSomething(): Unit = ???
+  //  }
+  //  case SECOND {
+  //    def doSomethingElse(): Unit = ???
+  //  }
+
+  sealed trait Cases
+  object Cases {
+    case class First {
+      def doSomething(): Unit = println("Something")
+    }
+    case class Second {
+      def doSomethingElse(): Unit = println("Something else")
+    }
+  }
 
 }
